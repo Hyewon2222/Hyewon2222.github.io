4842cbbb0d7d98d5ed081159335374c3e0aa307f
Hyewon2222 <gracegarden22@snu.ac.kr>
2026-05-03 15:01:22 +0000

    Console탭 인사말 변경

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -4,6 +4,6 @@ const major = "기록학 협동과정";
 const year = "2026";
 
 console.log(
-    `안녕하세요! ${author}(${major})의 사이트입니다.` +
-    `이 사이트는 ${year}년에 개설되었습니다.`
+    `안녕하세요! ${author}(${major})의 사이트입니다.
+    이 사이트는 ${year}년에 개설되었습니다.`
 );
