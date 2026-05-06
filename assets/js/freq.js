319d9121d4ae557e9aa3cfd0d6d4b22a71ea43d2
Hyewon2222 <gracegarden22@snu.ac.kr>
2026-05-06 07:33:55 +0000

    freq.js 오타 수정

--- assets/js/freq.js
+++ assets/js/freq.js
@@ -26,4 +26,4 @@ for (let i = 1; i < counts.length; i++) {
 }
 
 const top = targets[maxIdx];
-console.log(`가장 자주 나온 글자: '${top}' ${counts[maxIdx]번}`);
+console.log(`가장 자주 나온 글자: '${top}' ${counts[maxIdx]}번`);
