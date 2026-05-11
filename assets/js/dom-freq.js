039fb3b59a0b35aa75f4f372df49d35a0bcd57e8
Hyewon2222 <gracegarden22@snu.ac.kr>
2026-05-11 07:34:49 +0000

    dom-freq.js 오타 수정

--- assets/js/dom-freq.js
+++ assets/js/dom-freq.js
@@ -27,7 +27,7 @@ for (let i = 1; i < counts.length; i++) {
 }
 
 const topChar = document.querySelector("#top-char");
-topChar.textContent = `가장 자주 나온 글자: ${targets[maxIdx]} (${counts[mazIdx]}번)`;
+topChar.textContent = `가장 자주 나온 글자: ${targets[maxIdx]} (${counts[maxIdx]}번)`;
 topChar.computedStyleMap.fontWeight = "bold";
 topChar.computedStyleMap.color = "crimson";
 
