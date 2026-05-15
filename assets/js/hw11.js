3ae7bf6a9c2e6e06553e1029d3f1df4e24a3ab88
Hyewon2222 <gracegarden22@snu.ac.kr>
2026-05-15 11:12:39 +0000

    hw11의q2작성

--- assets/js/hw11.js
+++ assets/js/hw11.js
@@ -13,3 +13,21 @@ themeBtn.addEventListener("click", () => {
         themeBtn.textContent = "다크 모드";
     }
 });
+
+// Q2
+const q2Input = document.querySelector(#q2-input);
+const q2Count = document.querySelector(#q2-count);
+const q2Warn = document.querySelector(#q2-warn);
+
+q2Input.addEventListener("input", (e) => {
+    const currentLength = e.target.value.length;
+    q2Count.textContent = currentLength;
+
+    if (currentLength >= 100) {
+        q2Warn.textContent = "100자를 넘었습니다.";
+        q2Warn.style.color = "crimson";
+    } else {
+        q2Warn.textContent = "";
+    }
+
+});
